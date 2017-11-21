1fdab2297b8aafa59cefe130db1bc67288266aea
Courtney Phillips <courtney@epicodus.com>
2017-11-21 14:26:32 -0800

    Replace greeting with heading, remove clock, to begin transforming app into a Help Queue.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-function liveTime(){
-  var greeting = React.createElement('h1', {}, 'Hello, World!');
-  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-  var app = React.createElement('div', {}, greeting, clock);
+  var heading = React.createElement('h1', {}, 'Help Queue');
+  var app = React.createElement('div', {}, heading);
 
   ReactDOM.render(
     app,
     document.getElementById('react-app-root')
   );
-}
-
-setInterval(liveTime, 1000);
