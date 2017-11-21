8eb6b1e27d62b4127b0e2aa0dbe15ebf6961ca35
Courtney Phillips <courtney@epicodus.com>
2017-11-21 11:46:55 -0800

    Create parent React element to house both greeting and clock elements.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var greeting = React.createElement('h1', {}, 'Hello, World!');
+var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
 
 ReactDOM.render(
   greeting,
