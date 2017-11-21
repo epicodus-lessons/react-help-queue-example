eb9a9545072078bc7df040310e851a30b8bfdfa7
Courtney Phillips <courtney@epicodus.com>
2017-11-21 11:56:53 -0800

    House logic within liveTime() function.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-var greeting = React.createElement('h1', {}, 'Hello, World!');
-var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-var app = React.createElement('div', {}, greeting, clock);
+function liveTime(){
+  var greeting = React.createElement('h1', {}, 'Hello, World!');
+  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  var app = React.createElement('div', {}, greeting, clock);
 
-ReactDOM.render(
-  app,
-  document.getElementById('react-app-root')
-);
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+}
