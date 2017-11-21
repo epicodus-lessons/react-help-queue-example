357e12e86dd3f8aa47eb6e74dd98ee5754d3eb87
Courtney Phillips <courtney@epicodus.com>
2017-11-21 14:27:25 -0800

    Add and render ticketLocation, ticketNames, and ticketIssues elements to hard-code help queue ticket in UI.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
   var heading = React.createElement('h1', {}, 'Help Queue');
-  var app = React.createElement('div', {}, heading);
-
+  var ticketLocation = React.createElement('h3', {}, '3a');
+  var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+  var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
+  var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
+  
   ReactDOM.render(
     app,
     document.getElementById('react-app-root')
