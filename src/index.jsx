2591f50a8da2c902d1738a184dc19436fe876e02
Courtney Phillips <courtney@epicodus.com>
2017-11-21 14:33:11 -0800

    Link new JSX source code in index.html script tags.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
-  var heading = React.createElement('h1', {}, 'Help Queue');
-  var ticketLocation = React.createElement('h3', {}, '3a');
-  var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
-  var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
-  var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
-  
-  ReactDOM.render(
-    app,
-    document.getElementById('react-app-root')
-  );
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
