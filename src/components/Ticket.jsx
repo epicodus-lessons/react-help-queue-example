140a14ab8c39ddffcc3ab4f783a07b6224d659e2
Courtney Phillips <courtney@epicodus.com>
2017-11-30 15:01:46 -0800

    Install PropTypes library, declare PropTypes in Ticket component.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ticket(props){
   return (
@@ -9,5 +10,11 @@ function Ticket(props){
     </div>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string
+};
 
 export default Ticket;
