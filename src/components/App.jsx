f88881dff1b258f56cdb8e81dd7b6e6ed73e6214
Courtney Phillips <courtney@epicodus.com>
2017-11-30 11:45:41 -0800

    Add content to parent component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function App(){
-  return ();
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default App;
