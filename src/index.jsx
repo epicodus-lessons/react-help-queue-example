f8ee62ae6863f546ec08261a4aede608968da47a
Courtney Phillips <courtney@epicodus.com>
2017-11-30 11:49:47 -0800

    Update entry point file to render parent App component.

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3a</h3>
-    <h3>Thato and Haley</h3>
-    <p><em>Firebase will not save record!</em></p>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
