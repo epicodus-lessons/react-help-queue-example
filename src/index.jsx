0451a7d07e210ecdf3e4b8ad21da58a918be1445
Courtney Phillips <courtney@epicodus.com>
2017-12-15 13:38:08 -0800

    Add more ESLint rules from the React plugin.

--- src/index.jsx
+++ src/index.jsx
@@ -15,11 +15,9 @@ const render = (Component) => {
 render(App);
 
 /*eslint-disable */
-
 if (module.hot) {
   module.hot.accept('./components/App', () => {
     render(App);
   });
 }
-
 /*eslint-enable */
