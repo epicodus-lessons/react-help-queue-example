24f7d81ebd43defd5c67bb6efd96360c50558538
Courtney Phillips <courtney@epicodus.com>
2018-01-09 12:03:32 -0800

    Import NewTicketComponent into App component file, for use in routes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import NewTicketForm from './NewTicketForm';
 import { Switch, Route } from 'react-router-dom';
 
 function App(){
