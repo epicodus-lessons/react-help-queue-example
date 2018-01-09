a41cc1418ee267122f4a55e07540600c728cd323
Courtney Phillips <courtney@epicodus.com>
2018-01-09 11:45:45 -0800

    Add first route in Switch tags within parent component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
     <div>
       <Header/>
-      <TicketList />
+      <Switch>
+        <Route exact path="/" component={TicketList} />
+      </Switch>
     </div>
   );
 }
