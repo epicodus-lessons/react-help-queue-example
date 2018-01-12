e596c48d988401d180090f76843cd1670a455d08
Courtney Phillips <courtney@epicodus.com>
2018-01-12 12:19:39 -0800

    Set default state in NewTicketControl constructor.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 class NewTicketControl extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
   render(){
-    // We'll add to this a little later. 
+    // We'll add to this a little later.
   }
 }
 
