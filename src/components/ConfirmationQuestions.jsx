f3eb69c35ab67daed1fd64b6170e57624c2b91c9
Courtney Phillips <courtney@epicodus.com>
2018-01-12 15:19:04 -0800

    Create new ConfirmationQuestions components.

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationQuestions(){
+  return (
+    <div>
+      <p>Have you gone through all the steps on the Learn How to Program debugging lesson?</p>
+      <button>Yes</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestions;
