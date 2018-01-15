a9d959cc906513fdd3816bb4368994f4a21a1103
Courtney Phillips <courtney@epicodus.com>
2018-01-15 12:40:44 -0800

    Polish and proofread Creating Stateful Components lesson.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
 
 class NewTicketControl extends React.Component {
 
@@ -7,20 +8,17 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
-    this.setState({formVisibleOnPage: true});
-    console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
-  }
+  // handleClick(){
+  //   this.setState({formVisibleOnPage: true});
+  //   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+  // }
 
   render(){
     return (
-      <div>
-        <p>This is the NewTicketControl component!</p>
-        <strong onClick={this.handleClick}>Click me to change my state!</strong>
-      </div>
+      <ConfirmationQuestions />
     );
   }
 }
