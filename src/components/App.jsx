d324aa929a5fc0dc731bf77133df3497174894ac
Courtney Phillips <courtney@epicodus.com>
2018-01-17 13:00:52 -0800

    Refactor App component to be stateful, contain application state master ticket list.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,28 @@ import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketControl} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: [];
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={TicketList} />
+          <Route path='/newticket' component={NewTicketControl} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
