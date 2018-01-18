5d8489f272907dabc0f78ceed85deafd940501a6
Courtney Phillips <courtney@epicodus.com>
2018-01-18 14:33:46 -0800

    Call new moment method to display nicely-formatted time in Ticket component.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
 
 function Ticket(props){
   return (
     <div>
       <h3>{props.location} - {props.names}</h3>
+      <h4>{displayTimeOpen(props.timeOpened)}</h4>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
+}
+
+function displayTimeOpen(timeOpened) {
+  let now = new Moment(new Date());
+  let then = new Moment(timeOpened);
+  let stringTime = then.from(now, true);
+  return stringTime;
 }
 
 Ticket.propTypes = {
