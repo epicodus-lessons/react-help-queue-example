920d4308709ec056494740acb3acdd31b3818992
Courtney Phillips <courtney@epicodus.com>
2018-01-19 14:53:12 -0800

    Pass new formattedWaitTime values calculated in App component as prop to TicketList component.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -10,7 +10,7 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          timeOpen={ticket.timeOpen}
+          formattedWaitTime={ticket.formattedWaitTime}
           key={ticket.id}/>
       )}
     </div>
