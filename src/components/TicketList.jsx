a6c900653e2187c6ffdae50ce19b61c575a8f225
Courtney Phillips <courtney@epicodus.com>
2018-01-19 12:48:34 -0800

    Rename time property.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -10,7 +10,7 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          timeOpened={ticket.timeOpened}
+          timeOpen={ticket.timeOpen}
           key={ticket.id}/>
       )}
     </div>
