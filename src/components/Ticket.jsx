438e95fbf015727841d9a29ad543393a7ebb776f
Courtney Phillips <courtney@epicodus.com>
2018-01-19 14:54:24 -0800

    Refactor Ticket component to accept and display formattedWaitTime prop passed down from App instead of calculating wait times locally.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Moment from 'moment';
 
 function Ticket(props){
   return (
     <div>
       <h3>{props.location} - {props.names}</h3>
-      <h4>{displayTimeOpen(props.timeOpen)} ago</h4>
+      <h4>{props.formattedWaitTime}</h4>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
 }
 
-function displayTimeOpen(timeOpen) {
-  return timeOpen.from(new Moment(), true);
-}
-
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
-  timeOpen: PropTypes.instanceOf(Moment).isRequired
+  formattedWaitTime: PropTypes.string.isRequired,
 };
 
 export default Ticket;
