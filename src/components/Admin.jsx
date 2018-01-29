a56cbfde4eaabd5435fac489325d7abee56ea152
Courtney Phillips <courtney@epicodus.com>
2018-01-29 14:14:57 -0800

    Add proptypes to Admin component to account for new prop being passed from parent. Add argument to function declaration.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Admin(){
+function Admin(props){
   return (
     <div>
       <h2>This is the admin page!</h2>
     </div>
   );
 }
+
+Admin.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default Admin;
