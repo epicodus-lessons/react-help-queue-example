4083a649da0d090c00b47dc5b19220775edb5779
Courtney Phillips <courtney@epicodus.com>
2018-01-29 14:49:44 -0800

    Add PropType for currentRouterPath prop in Ticket component.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -17,6 +17,7 @@ Ticket.propTypes = {
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
   formattedWaitTime: PropTypes.string.isRequired,
+  currentRouterPath: PropTypes.string
 };
 
 export default Ticket;
