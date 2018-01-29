d6e59e1004e7bf3b9fb45717d3a214e46f70b54b
Courtney Phillips <courtney@epicodus.com>
2018-01-29 14:45:32 -0800

    Pass currentRouterPath prop from Admin to TicketList.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -6,7 +6,9 @@ function Admin(props){
   return (
     <div>
       <h2>Admin</h2>
-      <TicketList ticketList={props.ticketList} />
+      <TicketList
+        ticketList={props.ticketList}
+        currentRouterPath={props.currentRouterPath}/>
     </div>
   );
 }
