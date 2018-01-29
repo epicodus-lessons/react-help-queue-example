c4cfda232c9ccc4c5f0aaf634be725e3cb9e55a5
Courtney Phillips <courtney@epicodus.com>
2018-01-29 15:00:17 -0800

    Add conditional in Ticket component to include additional click listener only if rendered on Admin route.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Ticket(props){
-  return (
+  const ticketInformation =
     <div>
       <h3>{props.location} - {props.names}</h3>
       <h4>{props.formattedWaitTime}</h4>
       <p><em>{props.issue}</em></p>
       <hr/>
-    </div>
-  );
+    </div>;
+  if (props.currentRouterPath === '/admin'){
+    return (
+      <div onClick={() => {alert('hey, you just clicked the ticket belonging to ' + props.names)}}>
+        {ticketInformation}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {ticketInformation}
+      </div>
+    );
+  }
 }
 
 Ticket.propTypes = {
