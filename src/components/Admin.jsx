4b1652493cad0ea7b5f3b43504aec3f60580a784
Courtney Phillips <courtney@epicodus.com>
2018-01-31 10:57:02 -0800

    Render new TicketDetail component as child of Admin component.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
 
 function Admin(props){
   return (
     <div>
       <h2>Admin</h2>
+      <TicketDetail />
       <TicketList
         ticketList={props.ticketList}
         currentRouterPath={props.currentRouterPath}/>
