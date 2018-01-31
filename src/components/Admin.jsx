4b3a005ff6eb998cf4216e6ee250877cf1b05eb6
Courtney Phillips <courtney@epicodus.com>
2018-01-31 12:02:54 -0800

    Add conditional rendering to Admin to only display ticket details after a ticket has been selected.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -4,10 +4,14 @@ import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
 
 function Admin(props){
+  let optionalSelectedTicketContent = null;
+  if (props.selectedTicket != null){
+    optionalSelectedTicketContent =  <TicketDetail selectedTicket={props.selectedTicket}/>;
+  }
   return (
     <div>
       <h2>Admin</h2>
-      <TicketDetail />
+      {optionalSelectedTicketContent}
       <TicketList
         ticketList={props.ticketList}
         currentRouterPath={props.currentRouterPath}
