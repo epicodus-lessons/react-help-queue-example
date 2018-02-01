edbaa854503f68c614f4d37a5b5819e6e9778b14
Courtney Phillips <courtney@epicodus.com>
2018-01-31 16:16:17 -0800

    Update JSX to map through masterTicketList in its new format in TicketList component.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -6,21 +6,22 @@ function TicketList(props){
   return (
     <div>
       <hr/>
-      {props.ticketList.map((ticket) =>
+      {Object.keys(props.ticketList).forEach(ticketId => {
+        var ticket = props.ticketList[ticketId];
         <Ticket names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          formattedWaitTime={ticket.formattedWaitTime}
-          currentRouterPath={props.currentRouterPath}
-          key={ticket.id}
-          onTicketSelection={props.onTicketSelection}/>
-      )}
+        location={ticket.location}
+        issue={ticket.issue}
+        formattedWaitTime={ticket.formattedWaitTime}
+        currentRouterPath={props.currentRouterPath}
+        key={ticket.id}
+        onTicketSelection={props.onTicketSelection}/>;
+      })}
     </div>
   );
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array,
+  ticketList: PropTypes.object,
   currentRouterPath: PropTypes.string,
   onTicketSelection: PropTypes.func
 };
