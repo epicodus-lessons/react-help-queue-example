98477d5117cee2a174fc80b261e85eed86d6ca98
Courtney Phillips <courtney@epicodus.com>
2018-02-15 15:31:48 -0800

    Add Provider as ancestor of component tree, pass store as prop.

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,17 @@ import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import ticketListReducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
 
 const store = createStore(ticketListReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <HashRouter>
+    <HashRouter>
+      <Provider store={store}>
         <Component/>
-      </HashRouter>
-    </AppContainer>,
+      </Provider>
+    </HashRouter>,
     document.getElementById('react-app-root')
   );
 };
