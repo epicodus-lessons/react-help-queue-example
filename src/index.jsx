e705e5d14b797254d9514162d560d471744877db
Courtney Phillips <courtney@epicodus.com>
2018-02-15 15:27:03 -0800

    Create Redux store in entry point file.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import ticketListReducer from './reducers/ticket-list-reducer';
+
+const store = createStore(ticketListReducer);
 
 const render = (Component) => {
   ReactDOM.render(
