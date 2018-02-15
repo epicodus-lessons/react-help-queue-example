89bb373ad9cf9d12af3a0886005eee1e4956e2e2
Courtney Phillips <courtney@epicodus.com>
2018-02-15 15:05:29 -0800

    Add sample ticket information to testing file for use in specs.

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -2,6 +2,15 @@ import ticketListReducer from './../../src/reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
 
+  let action;
+  const sampleTicketData = {
+    names : 'Ryan & Aimen',
+    location : "4b",
+    issue : "Jest is being a diva and won't work with Webpack!",
+    timeOpen : 1500000000000,
+    id: 0
+  };
+
   test('Should return default state if no action type is recognized', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
