984e63916d69a4ddf4bf3c2960adfd225bab5a58
Courtney Phillips <courtney@epicodus.com>
2018-02-15 15:13:50 -0800

    Program ticketListReducer to anticipate and correctly handle ADD_TICKET actions.

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+  case 'ADD_TICKET':
+    const { names, location, issue, timeOpen, id } = action;
+    let newState = Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
+    return newState;
+  default:
+    return state;
+  }
 };
