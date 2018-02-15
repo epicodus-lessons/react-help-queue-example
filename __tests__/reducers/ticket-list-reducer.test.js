f549eb403f460334eb1615de37f04c20c5cd8fbd
Courtney Phillips <courtney@epicodus.com>
2018-02-15 15:09:38 -0800

    Write test asserting ticketListReducer can successfully save ticket data to Redux store.

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -15,4 +15,25 @@ describe('ticketListReducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
+  });
+
 });
