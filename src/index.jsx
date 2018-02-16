0e6eb830b1dea1c3350ce48879d4d6f247be5660
Courtney Phillips <courtney@epicodus.com>
2018-02-16 13:23:00 -0800

    Pass rootReducer to Redux store in entry point file.

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,9 @@ import { HashRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import ticketListReducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-const store = createStore(ticketListReducer);
+const store = createStore(rootReducer);
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
