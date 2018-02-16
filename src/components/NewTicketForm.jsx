5d2aa0f2b6694d2d73a7fdba1ff9210bdac47632
Courtney Phillips <courtney@epicodus.com>
2018-02-15 16:18:33 -0800

    Use UUID to assign unique IDs before Redux action is dispatched in NewTicketForm.

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Moment from 'moment';
 import { connect } from 'react-redux';
+import { v4 } from 'uuid';
 
 function NewTicketForm(props){
   let _names = null;
@@ -12,7 +13,7 @@ function NewTicketForm(props){
     event.preventDefault();
     const action = {
       type: 'ADD_TICKET',
-      id: null,
+      id: v4(),
       names: _names.value,
       location: _location.value,
       issue: _issue.value,
