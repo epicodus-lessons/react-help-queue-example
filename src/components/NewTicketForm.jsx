d64244178e7aa929c2f166ed8bd06d83cfbc2274
Courtney Phillips <courtney@epicodus.com>
2018-02-19 13:21:29 -0800

    Add Moment-formatted wait time to ADD_TICKET action paylod in NewTicketForm.

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -17,7 +17,8 @@ function NewTicketForm(props){
       names: _names.value,
       location: _location.value,
       issue: _issue.value,
-      timeOpen: new Moment()
+      timeOpen: new Moment(),
+      formattedWaitTime: new Moment().fromNow(true)
     };
     dispatch(action);
     _names.value = '';
