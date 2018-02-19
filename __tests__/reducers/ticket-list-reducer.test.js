4ceb40466d166afbfcadf4d1c5d562e6d1c70114
Courtney Phillips <courtney@epicodus.com>
2018-02-19 13:20:35 -0800

    Write test confirming new tickets added by ADD_TICKET action include Moment-formatted formattedWaitTime value.

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -1,4 +1,5 @@
 import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+import Moment from 'moment';
 
 describe('ticketListReducer', () => {
 
@@ -32,6 +33,29 @@ describe('ticketListReducer', () => {
         issue: issue,
         timeOpen: timeOpen,
         id: id
+      }
+    });
+  });
+
+  test('New ticket should include Moment-formatted wait times', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id,
+      formattedWaitTime: new Moment().fromNow(true)
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: 'a few seconds'
       }
     });
   });
