cdf2229012402eb4000a4e3ee876b5743197295f
Courtney Phillips <courtney@epicodus.com>
2018-02-22 12:06:11 -0800

    Apply persistDataLocally middleware to Redux store in entry point.

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import persistDataLocally from './middleware/persist-data-locally';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(persistDataLocally));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
