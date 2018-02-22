5140a035a09aca9420902f687d5120414893df36
Courtney Phillips <courtney@epicodus.com>
2018-02-22 13:03:36 -0800

    Rehydrate data stored in client's local storage into Redux store in entry point.

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,18 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 import persistDataLocally from './middleware/persist-data-locally';
 
-const store = createStore(rootReducer, applyMiddleware(persistDataLocally));
+let retrievedState;
+try {
+  retrievedState = localStorage.getItem('reduxStore');
+  if (retrievedState === null){
+    retrievedState = {};
+  }
+  retrievedState = JSON.parse(retrievedState);
+} catch (err){
+  retrievedState = {};
+}
+
+const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
