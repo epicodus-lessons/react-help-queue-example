ea8df59f7f146b71656f99ff30bf0c07073e5702
Courtney Phillips <courtney@epicodus.com>
2018-02-22 12:07:01 -0800

    Refactor persistDataLocally middleware to call next() before saving data to store, so reducer has chance to process most recent ticket.

--- src/middleware/persist-data-locally.js
+++ src/middleware/persist-data-locally.js
@@ -1,7 +1,7 @@
 const persistDataLocally = store => next => action => {
-  localStorage['reduxStore'] = JSON.stringify(store.getState());
-  console.log('Local Storage:', localStorage['reduxStore']);
-  return next(action);
+  next(action);
+  localStorage["reduxStore"] = JSON.stringify(store.getState());
+  console.log("Local Storage:", localStorage["reduxStore"]);
 }
 
 export default persistDataLocally;
