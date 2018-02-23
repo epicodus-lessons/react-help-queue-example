26b352eed3a11ab51200b3bc89cc628f91b046fa
Staff <epicodus_staff@Staff.local>
2018-02-23 09:32:20 -0800

    add firebaseConfig to constants folder

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCRh5QS5mHbP5GxkKQxzwR8fYiiqffiD8k",
+  authDomain: "help-queue-3cffb.firebaseapp.com",
+  databaseURL: "https://help-queue-3cffb.firebaseio.com",
+  projectId: "help-queue-3cffb",
+  storageBucket: "help-queue-3cffb.appspot.com",
+  messagingSenderId: "902149280910"
+};
