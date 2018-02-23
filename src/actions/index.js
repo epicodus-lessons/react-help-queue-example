8618b3113c7ad920d42e17c78a090fa2cb9e62a7
Staff <epicodus_staff@Staff.local>
2018-02-23 09:46:55 -0800

    install firebase and corrisponding action file

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+import constants from './../constants';
+const { firebaseConfig, types } = constants;
+import firebase from 'firebase';
+
+
+firebase.initializeApp(firebaseConfig);
+const tickets = firebase.database().ref('tickets');
+
+export function addTicket(_names, _location, _issue) {
+  return () => tickets.push({
+    names: _names,
+    location: _location,
+    description: _issue,
+    timeOpened: new Date().getTime()
+  })
+}
+
+function receiveTicket(ticketFromFirebase) {
+  return {
+    type: types.RECEIVE_TICKET,
+    ticket: ticketFromFirebase
+  };
+}
+
+export function subscribeToTickets() {
+  return function(dispatch) {
+    tickets.on('child_added', data => {
+      console.log('in subscribe:');
+      console.log(data.val());
+      dispatch(receiveTicket(data.val()))
+    });
+  }
+}
