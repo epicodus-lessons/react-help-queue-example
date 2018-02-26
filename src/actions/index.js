c4e37d234a54c044522d35722c7d9683e4a2a334
Courtney Phillips <courtney@epicodus.com>
2018-02-26 11:15:45 -0800

    Define new addTicket() action creator to save tickets to Firebase.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,12 @@ import firebase from 'firebase';
 
 firebase.initializeApp(firebaseConfig);
 const tickets = firebase.database().ref('tickets');
+
+export function addTicket(_names, _location, _issue) {
+  return () => tickets.push({
+    names: _names,
+    location: _location,
+    issue: _issue,
+    timeOpen: new Date().getTime()
+  });
+}
